1cbc363d8a1b73372eb8952ef1086f232104b323
Kashishrao <kashishrao40@gmail.com>
2018-06-03 23:42:42 -0400

    lotto demo class created

---
+++ Exercise2/LottoDemo.java
@@ -0,0 +1,27 @@
+package Exercise2;
+
+import javax.swing.*;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+
+public class LottoDemo {
+    public static void main (String[] args) {
+
+        // create a Lottery object
+        Lotto lotto = new Lotto();
+
+        String lottoStr = JOptionPane.showInputDialog("Enter number between 3 to 27 to get Lotto: ");
+        int lotto_num=Integer.parseInt(lottoStr);
+        // tell the user how many numbers they matched
+        int matches = lotto.addAllLottoNumber();
+        if (matches == lotto_num) {
+            JOptionPane.showMessageDialog(null, "You Win Lotto " );
+        }  else {
+            JOptionPane.showMessageDialog(null, "You Lost Lotto and Lotto is:"+matches );
+        }
+
+    }
+
+
+}
